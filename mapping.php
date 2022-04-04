5295d79365c695409caf8f5c71df01f7d74d48d4
MrZANO <mrzano.spam@gmail.com>
2022-04-04 12:12:13 +0200

    Update mapping.php

--- mapping.php
+++ mapping.php
@@ -16,6 +16,28 @@ foreach ($valorexpath as $key => $value) {
 	echo $url_nuova_ricerca;
 	//test
 	echo "</br>";
-	
+	///url prova
+	$url_nuova_ricerca ="https://www.carpisa.it/it_it/BSA49201442?_q=BSA49201442&map=ft&page=1";
 	//fare Curl con url_nuova_ricerca
+$ch = curl_init($url_nuova_ricerca);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+$result = curl_exec($ch);
+
+echo htmlentities($result);
+
+//estrarre tutti gli URL
+
+
+//$ch = curl_init($url_nuova_ricerca);
+//curl_setopt($ch, CURLOPT_HEADER, 1);
+//$response = curl_exec($ch);
+//$response = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
+//     $start = stripos($response, "<body");
+//     $end = stripos($response, "</body");
+//
+//     $body = substr($response,$start,$end-$start);
+//	 
+//echo ($body);
 }
